7565f79a06f789d4d370f0a282de25ab9c3de8e7
only-gooooose <jeremy.sjx@gmail.com>
2025-02-13 20:52:03 +0800

    Site updated: 2025-02-13 20:52:02

--- js/diytitle.js
+++ js/diytitle.js
@@ -1,1 +1,19 @@
+//动态标题
+var OriginTitile = document.title;
+var titleTime;
+document.addEventListener('visibilitychange', function () {
+    if (document.hidden) {
+        //离开当前页面时标签显示内容
+        document.title = '超级鸽子王的小破站';
+        clearTimeout(titleTime);
+    }
+    else {
+        //返回当前页面时标签显示内容
+        document.title = 'goooooose的小破站' + OriginTitile;
+        //两秒后变回正常标题
+        titleTime = setTimeout(function () {
+            document.title = OriginTitile;
+        }, 2000);
+    }
+});
  
